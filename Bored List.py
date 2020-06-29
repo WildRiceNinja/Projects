05bbdd0f445c5f65b2b3f9156cfe3d9662050840
WildRiceNinja <67569360+WildRiceNinja@users.noreply.github.com>
2020-06-28 23:36:46 -0400

    Add files via upload

---
+++ Bored List.py
@@ -0,0 +1,27 @@
+exit = False
+while exit == False:
+    print('what do you want to do?\n'
+            'Add?\n'
+            'Check?\n'
+            'Clear?\n'
+            'Exit?')
+    inpt = input()
+    if inpt == "Exit":
+        print("Exited")
+        exit = True
+    elif inpt == "Add":
+        print("what do you want to add?")
+        while True:
+            todo = input()
+            if todo == 'Exit':
+                break
+            else:
+                with open('To-Do list', "a+") as List:
+                    List.write(todo + "\n")
+    elif inpt == "Check":
+        with open('To-Do list', "r") as rList:
+            print(rList.read())
+    #elif inpt == "Clear":
+
+        
+
